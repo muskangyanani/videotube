82779d8764d6138b98a377b64de203ee2b4f4aa2
muskangyanani <muskaann.129@gmail.com>
2025-01-28 20:49:23 +0530

    standardised error and api response utils

---
+++ src/utils/asyncHandlers.js
@@ -0,0 +1,9 @@
+const asyncHandler = (requesHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requesHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+export { asyncHandler };
